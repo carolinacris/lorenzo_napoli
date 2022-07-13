203a72f950830b8fce075cc5543fc9a0a5663db4
Carolina Cristino <carolinacristino.studia@mohole.com>
2022-07-13 16:28:48 +0200

    creazione enviroments

--- lorenzo_napoli_code/src/components/enviroment/enviroment.jsx
+++ lorenzo_napoli_code/src/components/enviroment/enviroment.jsx
@@ -2,8 +2,7 @@ import "./enviroment.css"
 export const Enviroment = ()=>{
     return(
         <div className="card">
-            <div className="png-wrap">
-                <img src="src/assets/img/cards/enviroments/env1.jpg" alt="env1" />
+            <div className="png-wrap img">
             </div>
                 <p className="titolo">Second Law - Cosmo</p>
         </div>
