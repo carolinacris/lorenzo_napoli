3b91aa411616eac6cb017210ab448904996f7f1a
Carolina Cristino <carolinacristino.studia@mohole.com>
2022-07-18 12:45:31 +0200

    adding skull

--- lorenzo_napoli_code/src/pages/home/Homie.jsx
+++ lorenzo_napoli_code/src/pages/home/Homie.jsx
@@ -1,10 +1,11 @@
 import { Nav } from "../../components/nav/Nav";
+import Spline from '@splinetool/react-spline';
 import "./home.css"
 export const Homie = () => {
   return (
     <section className="home">
       <Nav ></Nav>
-      <div className="canvas"></div>
+      <Spline scene="https://prod.spline.design/sMLyVl4gMphPuvpv/scene.splinecode" />
       <div className="texbox">
         <p>Frease che dovrà essere spdijspdijwpdjsjjsfpowaj+apj sndd w apjdcpj dkdjdjdj A APC presa dal cms come intro figa di quello che fa lollo</p>
       </div>
