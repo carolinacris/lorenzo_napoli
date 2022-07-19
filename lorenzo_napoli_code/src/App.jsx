9c0e0a49af3a531c1e4159211ef11409fb865d88
d <error404@localhost.localdomain>
2022-07-19 21:10:36 +0200

    sparkling, sparkling water mixed with peaches and rum

--- lorenzo_napoli_code/src/App.jsx
+++ lorenzo_napoli_code/src/App.jsx
@@ -1,5 +1,5 @@
-import { Routes, Route } from "react-router-dom";
-import { Homie } from "./pages/home/homie";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { Homie } from "./pages/home/Homie";
 import { Header } from "./components/header/Header";
 import {Charaters} from "./pages/charaters/Charaters"
 import {Chat} from "./pages/chat/Chat"
@@ -7,16 +7,26 @@ import { Footer } from "./components/footer/Footer";
 import { Enviroments } from "./pages/enviroments/enviroments";
 
 
+const RoutePaths = {
+    Index: "/",
+    Charaters: "/charaters",
+    Chat: "/chat",
+    Enviroments: "/enviroments",
+}
+
 export const App = () => {
-    
+    let location = useLocation();
+
+    const hasHeader = location.pathname !== RoutePaths.Chat;
+
     return (
         <>
-            <Header/>
+            {hasHeader && <Header/>}
             <Routes>
-                <Route path="/" element={<Homie />} />
-                <Route path="/charaters" element={<Charaters/>} />
-                <Route path="/chat" element={<Chat/>} />
-                <Route path="/enviroments" element={<Enviroments/>} />
+                <Route path={RoutePaths.Index} element={<Homie />} />
+                <Route path={RoutePaths.Charaters} element={<Charaters/>} />
+                <Route path={RoutePaths.Chat} element={<Chat/>} />
+                <Route path={RoutePaths.Enviroments} element={<Enviroments/>} />
             </Routes>
             <Footer></Footer>
           
